55faf379d6c6c20f74b6363097ecd7c83936a6c6
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-20 18:55:37 -0300

    Change back to preferred approach
    This reverts commit 8f1574902849c93cb0ffd2a1bc00701d920f7c68.

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 
 test('render one row per user', () => {
@@ -6,10 +6,9 @@ test('render one row per user', () => {
     { name: 'Jane', email: 'jane@example.com' },
     { name: 'John', email: 'john@example.com' },
   ];
-  const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
 
-  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
-  const rows = container.querySelectorAll('tbody tr');
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
   expect(rows).toHaveLength(users.length);
 });
 
