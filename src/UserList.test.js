16d3e2ede471b696d2738f164695411e8175e88b
Marcio Woitek <mwoitekjr@gmail.com>
2023-04-20 19:52:38 -0300

    Add setup function

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,23 +1,24 @@
 import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 
-test('render one row per user', () => {
+function renderComponent() {
   const users = [
     { name: 'Jane', email: 'jane@example.com' },
     { name: 'John', email: 'john@example.com' },
   ];
   render(<UserList users={users} />);
+  return { users };
+}
+
+test('render one row per user', () => {
+  renderComponent();
 
   const rows = within(screen.getByTestId('users')).getAllByRole('row');
-  expect(rows).toHaveLength(users.length);
+  expect(rows).toHaveLength(2);
 });
 
 test('render the name and email of each user', () => {
-  const users = [
-    { name: 'Jane', email: 'jane@example.com' },
-    { name: 'John', email: 'john@example.com' },
-  ];
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   for (let user of users) {
     const name = screen.getByRole('cell', {
